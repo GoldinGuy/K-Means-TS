c83c55d9b002a7b5b0f46c8585ba95ff50a40d2c
Seth <sethgoldin27@gmail.com>
2021-03-22 16:39:52 -0400

    update it count

--- test/kmeans.test.ts
+++ test/kmeans.test.ts
@@ -40,14 +40,14 @@ const input_data_single = [
 ];
 
 describe('test_kmeans', () => {
-  it('successfully generates clusters w/ 1 iteration', () => {
+  it('successfully generates clusters w/ max iterations', () => {
     expect(
       kmeans(input_data_single, 2, [
         [3, 1, 5],
         [7, 2, 6],
         [3, 8, 6],
       ])
-    ).toHaveProperty('iterations', 1);
+    ).toHaveProperty('iterations', 10000);
   });
 });
 
